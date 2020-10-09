14357089194a557fee19d99792baa1c4cc1943a7
asteriosfm2907 <72600853+asteriosfm2907@users.noreply.github.com>
2020-10-09 18:23:46 +0300

    second commit

---
+++ precache-manifest.0b126a0d27840956696e36b872e14d70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6671418a4d3bb95ef24c",
+    "url": "project/css/app.23d27409.css"
+  },
+  {
+    "revision": "91c0784cecdbcb3badf85ba842de5f21",
+    "url": "project/fonts/Montserrat-Medium.91c0784c.ttf"
+  },
+  {
+    "revision": "f94c71e147e98aafcf0beb317148e037",
+    "url": "project/index.html"
+  },
+  {
+    "revision": "6671418a4d3bb95ef24c",
+    "url": "project/js/app.066a78ce.js"
+  },
+  {
+    "revision": "6890aeec084d10df532f",
+    "url": "project/js/chunk-vendors.c08a6895.js"
+  },
+  {
+    "revision": "cac439e5627fd04e389de3e7dd18fecb",
+    "url": "project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "project/robots.txt"
+  }
+]);
